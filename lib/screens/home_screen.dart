0f3369980164906ff2bfeedd4354f99d66db2f3f
Jossue <jossue2598@gmail.com>
2022-01-25 21:03:36 -0500

    scaffold whit buildcontext

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,12 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  //Build context sabe que es lo que trae, es decir si lo llamamos desde el main sabremos desde donde venimos
   Widget build(BuildContext context) {
-    return const Center(
+    return const Scaffold(
+      body: Center(
         child: Text("Hola mundo desde home screen"),
-      );
-  } 
+      ),
+    );
+  }
 }
