04bd897b497aeccec8369c8d706059267c92c97a
Jossue <jossue2598@gmail.com>
2022-01-29 18:36:08 -0500

    creando primer state full widget

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
   @override
   //Build context sabe que es lo que trae, es decir si lo llamamos desde el main sabremos desde donde venimos
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Home Screen"),
-        elevation: 1.05,
+        title: const Text("Counter Screen"),
+        elevation: 1111.05,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
+          children: [
+            const Text(
               'Clicks Counter',
               style: fontSize30,
             ),
             Text(
-              '10',
+              '$counter',
               style: fontSize30,
             )
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // print('hola');
+          counter++;
+
+          setState(() {});
         },
         child: const Icon(Icons.plus_one_sharp),
       ),
-      
     );
   }
 }
